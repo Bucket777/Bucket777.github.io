cb331c1242dd38d5897b4780da6b45c899df1170
MarnoC <marnoc@gmail.com>
2019-05-29 20:25:53 +0200

    More changes to contact icons

---
+++ scripts/scripts.js
@@ -0,0 +1,24 @@
+function doSnackbar() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 1000);
+}
+
+function copyToClipboard() {
+    var dummy = document.createElement("textarea");
+    // to avoid breaking orgain page when copying more words
+    // cant copy when adding below this code
+    // dummy.style.display = 'none'
+    document.body.appendChild(dummy);
+    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+    dummy.value = 'marnoc@gmail.com';
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+    doSnackbar();
+}
